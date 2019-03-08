7460a4c938fd64e536dc66b280afb1b0b5337b96
bazeeel <deakfefe77@gmail.com>
2019-03-08 18:37:20 +0100

    nothing

--- startup.sh
+++ startup.sh
@@ -13,7 +13,7 @@ xrandr --output DP-2 --primary --mode 1920x1080 --rate 60.00
 #xrandr --output LVDS1 --mode 1366x768 --output DP3 --mode 1920x1080 --right-of LVDS1
 #xrandr --output HDMI1 --mode 1920x1080 --pos 1920x0 --rotate normal --output HDMI1 --primary --mode 1920x1080 --pos 0x0 --rotate normal --output VIRTUAL1 --off
 
-$HOME/.config/herbstluftwm/polybar/launch.sh &
+$HOME/.config/herbstluftwm/launch.sh &
 
 #change your keyboard if you need it
 setxkbmap -layout hu
@@ -22,7 +22,7 @@ setxkbmap -layout hu
 #feh --bg-scale ~/.xmonad/wall1.jpg &
 
 #conky -c $HOME/.xmonad/scripts/system-overview &
-#run variety &
+run variety &
 run nm-applet &
 #run pamac-tray &
 #run xfce4-power-manager &
